42f3937243543e7cb8907c5302ea05b7a0e009d1
feisabel <fernandampi@yahoo.com.br>
2016-08-29 13:56:38 -0400

    nem sei

---
+++ Android_Client-Authorization/app/src/main/java/ExternalData/DataRequestTest.java
@@ -0,0 +1,15 @@
+package ExternalData;
+/**
+ * Created by feisabel on 8/29/16.
+ */
+
+import org.junit.Test;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class DataRequestTest {
+    @Test
+
+}
+
